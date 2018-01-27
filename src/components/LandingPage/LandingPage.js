dc452ae73a5560e5a9a9d1fcb663412b5fd32baa
Steven Isbell <steven.isbell18@gmail.com>
2018-01-27 15:17:31 -0600

    wrapping up

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./LandingPage.css";
 
-const LandingPage = () => "Landing Page!";
+const LandingPage = () => (
+  <div>
+    <h1>Landing Page!</h1>
+  </div>
+);
 
 export default LandingPage;
